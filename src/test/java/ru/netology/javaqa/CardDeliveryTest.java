e28e0546ec1673294eccfc815ce6ee9fd5db51fe
Natalia Krasnykh <krasnih.n161@mail.ru>
2024-12-09 23:53:46 +0300

    Allure

--- src/test/java/ru/netology/javaqa/CardDeliveryTest.java
+++ src/test/java/ru/netology/javaqa/CardDeliveryTest.java
@@ -1,8 +1,11 @@
 package ru.netology.javaqa;
 
 import com.codeborne.selenide.Condition;
-
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -14,6 +17,17 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CardDeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
 
     @BeforeEach
     void setup() {
